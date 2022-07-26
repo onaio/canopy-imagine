8cac3d5cc3098c7406ca80a2a6956e7afb344fae
apietrobon@ona.io <apietrobon@ona.io>
2022-07-26 15:57:03 +0000

    grant select tweaks

--- macros/grant_select.sql
+++ macros/grant_select.sql
@@ -3,14 +3,13 @@
   {% if target.name == 'default' %}     {# runs only if the target for the job is the dbt schema, therefore only if the job is running in prod #}
 
   {% set sql %}
-  grant usage on schema {{ schema }} to  {{ user }};
-  grant select on all tables in schema {{ schema }} to  {{ user }};
+  grant usage on schema {{ schema }} to {{ user }};
+  grant select on all tables in schema {{ schema }} to {{ user }};
   {% endset %}
 
   {{ log('Granting select on all tables and views in schema ' ~ target.schema ~ ' to role ' ~ role, info=True) }}
   {% do run_query(sql) %}
   {{ log('Privileges granted', info=True) }}
 
-  {% endif %}  
-
+  {% endif %} 
 {% endmacro %}
