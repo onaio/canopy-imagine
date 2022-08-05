022ce2bf0acdcfbe792f4de953b6a9874dac1b00
apietrobon@ona.io <apietrobon@ona.io>
2022-08-05 10:35:59 +0000

    session daily update

--- models/marts/session_fulfillment_daily.sql
+++ models/marts/session_fulfillment_daily.sql
@@ -21,8 +21,8 @@ select
 	SUM(se.duration) as actual_mins,
 	AVG((cm.value::int)*ce.children) as expected_mins
 from {{ref('stg_term_days')}} td
-left join {{ref('stg_term_weeks')}} tw on date_trunc('week',(td.day::date)) = tw.week 
 left join {{ref('sessions')}} se on date_trunc('day',(se.start_time::date)) = td.day and td.term_id = se.term_id 
+left join {{ref('stg_term_weeks')}} tw on date_trunc('week',(td.day::date)) = tw.week  and tw.term_id = se.term_id 
 left join {{ref('stg_country_metrics')}} cm on cm.country = se.country
 left join child_enrollment ce on ce.term_id = se.term_id and ce.location_id = se.location_id 
 group by 1,2,3,4,5,6,7,8
