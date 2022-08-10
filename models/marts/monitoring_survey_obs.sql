093a072f5c86b1183a62f456bc7ca73d80c10659
apietrobon@ona.io <apietrobon@ona.io>
2022-08-10 14:09:36 +0000

    monitoring_survey_obs.sql

--- models/marts/monitoring_survey_obs.sql
+++ models/marts/monitoring_survey_obs.sql
@@ -6,9 +6,9 @@ select
     country,
     admin_3_name, 
     observation_date,
-    unnest(array['1. Setup', '2. During session', '3. Closing']) as observation_time,
-    unnest(array[setup_observations, tablet_use_observations, closing_observations]) as observations
+    unnest(array['1. Setup', '2. During session', '3. Closing', '4. Other observations']) as observation_time,
+    unnest(array[setup_observations, tablet_use_observations, closing_observations,other_session_obs]) as observations
 from {{ref('monitoring_survey')}}
 )
 select * from data 
-where observations is not null and observations != 'na'
+where observations is not null and observations not in ('na', '	N/A')
