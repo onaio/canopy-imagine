0af30664fd83b0cc8c125c53107e9e37ba623558
apietrobon@ona.io <apietrobon@ona.io>
2022-08-10 06:28:59 +0000

    monitoring_survey_obs

---
+++ models/marts/monitoring_survey_obs.sql
@@ -0,0 +1,11 @@
+select 
+    submission_id, 
+    observation_date,
+    field_officer,
+    location, 
+    country,
+    admin_3_name, 
+    observation_date,
+    unnest(array['setup', 'tablet_use', 'closing']) as observation_time,
+    unnest(array[setup_observations, tablet_use_observations, closing_observations]) as observations
+from {{ref('monitoring_survey')}}
