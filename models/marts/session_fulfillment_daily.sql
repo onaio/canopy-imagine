d56a4ba54811d3cc0e70541fff21fb3014ebb359
apietrobon@ona.io <apietrobon@ona.io>
2022-08-11 15:15:05 +0000

    session fulfillment updates

--- models/marts/session_fulfillment_daily.sql
+++ models/marts/session_fulfillment_daily.sql
@@ -18,14 +18,15 @@ select
 	se.location_id,
 	se.location,
 	se.country,
-	SUM(se.duration) as actual_mins,
-	AVG((cm.value::int)*ce.children) as expected_mins
+	ce.children,
+	SUM(se.duration/60) as actual_mins,
+	AVG(cm.value::int) as expected_mins
 from {{ref('stg_term_days')}} td
 left join {{ref('sessions')}} se on date_trunc('day',(se.start_time::date)) = td.day and td.term_id = se.term_id 
 left join {{ref('stg_term_weeks')}} tw on date_trunc('week',(td.day::date)) = tw.week  and tw.term_id = se.term_id 
 left join {{ref('stg_country_metrics')}} cm on cm.country = se.country
 left join child_enrollment ce on ce.term_id = se.term_id and ce.location_id = se.location_id 
-group by 1,2,3,4,5,6,7,8
+group by 1,2,3,4,5,6,7,8,9
 ) 
 
 select 
@@ -37,8 +38,9 @@ select
 	location_id,
 	location,
 	country,
+	children,
 	actual_mins,
 	expected_mins,
-	case when actual_mins >= expected_mins then 1 else 0 end as fullfillment
+	case when actual_mins >= expected_mins*children then 1 else 0 end as fullfillment 
 from actual_vs_expected
 where location_id is not null
