dbd4cb6ccc91c763227f186eb146a4278861984d
Bella Okiddy <bokiddy@ona.io>
2022-08-30 02:59:55 +0000

    Session table updates

--- models/marts/session_fulfillment_daily.sql
+++ models/marts/session_fulfillment_daily.sql
@@ -18,6 +18,8 @@ select
 	se.location_id,
 	se.location,
 	se.country,
+	se.field_officer,
+	se.is_last_week,
 	ce.children,
 	SUM(se.duration/60) as actual_mins,
 	AVG(cm.value::int) as expected_mins
@@ -26,7 +28,7 @@ left join {{ref('sessions')}} se on date_trunc('day',(se.start_time::date)) = td
 left join {{ref('stg_term_weeks')}} tw on date_trunc('week',(td.day::date)) = tw.week  and tw.term_id = se.term_id 
 left join {{ref('stg_country_metrics')}} cm on cm.country = se.country
 left join child_enrollment ce on ce.term_id = se.term_id and ce.location_id = se.location_id 
-group by 1,2,3,4,5,6,7,8,9
+group by 1,2,3,4,5,6,7,8,9,10,11
 ) 
 
 select 
@@ -38,6 +40,8 @@ select
 	location_id,
 	location,
 	country,
+	field_officer,
+	is_last_week,
 	children,
 	actual_mins,
 	expected_mins,
