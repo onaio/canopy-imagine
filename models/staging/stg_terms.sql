0f1993fbc1c08a4be9663ff7f3a34dbf24691800
Ahmad-Fuad <ahmed.bashewith@gmail.com>
2023-10-12 19:00:46 +0300

    added directus tables in source config and replaced stg_terms with directus data

--- models/staging/stg_terms.sql
+++ models/staging/stg_terms.sql
@@ -1,18 +1,24 @@
 with latest_term_per_country as (
     select
-    country, max(id) filter (where start_date::date <= now()::date ) as latest_term
-    from  {{source('airbyte', 'terms')}}
+    c.name as country,
+    max(t.id) filter (where start_date::date <= now()::date ) as latest_term
+    from  {{source('directus', 'terms')}} t
+    left join {{source('directus', 'partners_countries')}} pc on pc.id = t.partner_country_id
+    left join {{source('directus', 'countries')}} c on c.id = pc.countries_id
     group by 1
 )
+
+
 select 
-id::int,
-partner_id,
-rtrim(t.country) as country,
-rtrim(name) as name,
-start_date::date,
-end_date::date,
-case when lt.latest_term is not null then true else false end as latest_term,
-case when (now()::date >= start_date::date and now()::date <= end_date::date) then true else false end as  current_term
-from 
-{{source('airbyte', 'terms')}} t
+    t.id::int,
+    partner_country_id as partner_id,
+    rtrim(c.name) as country,
+    rtrim(t.name) as name,
+    start_date::date,
+    end_date::date,
+    case when lt.latest_term is not null then true else false end as latest_term,
+    case when (now()::date >= start_date::date and now()::date <= end_date::date) then true else false end as  current_term
+from {{source('directus', 'terms')}} t
 left join latest_term_per_country lt  on t.id = lt.latest_term
+left join {{source('directus', 'partners_countries')}} pc on pc.id = t.partner_country_id
+left join {{source('directus', 'countries')}} c on c.id = pc.countries_id
