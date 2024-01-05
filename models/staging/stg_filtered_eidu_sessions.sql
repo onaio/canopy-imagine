5bfa8fc19b258643bbeb7fb48032c9d0a7fa0a72
Ahmad-Fuad <ahmed.bashewith@gmail.com>
2024-01-05 09:59:37 +0300

    rectified syntax error to accomodate incremental

--- models/staging/stg_filtered_eidu_sessions.sql
+++ models/staging/stg_filtered_eidu_sessions.sql
@@ -11,10 +11,8 @@ left join {{ ref('stg_devices_per_week') }} dtd
     on 
     (s.device_id = dtd.device_id ) 
        and date_trunc('week', s.start_time::timestamp)::date = dtd.week
-where dtd.location_id not in (select location_id::int from {{source('data_corrections', 'schools_data_removal')}} )
-
-{% if is_incremental() %}
-
-  where s._airbyte_emitted_at > (select max(_airbyte_emitted_at) from {{ this }})
-
-{% endif %}
+where 
+    dtd.location_id not in (select location_id::int from {{source('data_corrections', 'schools_data_removal')}} )
+    {% if is_incremental() %}
+        and s._airbyte_emitted_at > (select max(_airbyte_emitted_at) from {{ this }})
+    {% endif %}
