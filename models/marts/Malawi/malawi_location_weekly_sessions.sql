6f60f3aba6467881ce7c383cebc71495a19ac5bc
Ahmad-Fuad <ahmed.bashewith@gmail.com>
2024-02-16 11:57:04 +0300

    added rolling metric to malawi locations sessions weekly marts

--- models/marts/Malawi/malawi_location_weekly_sessions.sql
+++ models/marts/Malawi/malawi_location_weekly_sessions.sql
@@ -1,3 +1,6 @@
-select *
-from {{ref("locations_weekly_sessions")}}
+select
+    ws.*,
+    coalesce(wrm.rolling_session_rating,0) as rolling_session_rating
+from {{ref("locations_weekly_sessions")}} ws
+left join {{ref("location_weekly_rolling_metrics")}} wrm on ws.location_id = wrm.location_id and ws.week = wrm.week
 where country = 'Malawi'
