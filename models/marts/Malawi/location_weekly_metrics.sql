50f91a4505d8628b62ff9b81c08731a3a4a6086e
Ahmad-Fuad <ahmed.bashewith@gmail.com>
2024-02-28 10:01:55 +0300

    minor syntax fix

--- models/marts/Malawi/location_weekly_metrics.sql
+++ models/marts/Malawi/location_weekly_metrics.sql
@@ -12,4 +12,4 @@ select
     avg(minutes_used::int) as minutes_used,
     avg(absent_students::int) as percentage_absent
 from {{ref("weekly_monitoring_survey")}}
-group by 1,2,3,4,5,6,7,8,9
+group by 1,2,3,4,5,6,7,8,9,10
